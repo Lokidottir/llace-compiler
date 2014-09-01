fa17c0e3713a80d7a031460ce1a1b0fe0fdcfa27
Lokidottir <mari.harald.uk@gmail.com>
2014-09-01 13:00:05 +0100

    Continued work on EBNF interpreter

--- proof-of-concept-cpp/src/main.cpp
+++ proof-of-concept-cpp/src/main.cpp
@@ -2,12 +2,26 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <cstring>
 #include <vector>
 #include "generic-btree.hpp"
 
+std::string stripSubstr(const std::string& content, const std::string& toStrip) {
+	std::string altered_content(content);
+	for (unsigned int i = 0; i < content.size() && i != std::string::npos; i = content.find(toStrip,i)) {
+		altered_content.erase(i, i + toStrip.size());
+	}
+	return altered_content;
+}
 
 int main(int argc, char** args) {
 	std::cout << "Circuit language compiler." << std::endl;
-	BNode<std::string> tree();
+	std::string ebnf_filename;
+	for (int i = 0; i < argc - 1; i++) {
+		if (strcmp(args[i], "-ebnf") == 0) {
+			ebnf_filename = stripSubstr(stripSubstr(args[i + 1], "\""),"'");
+		}
+	}
+	
 	return 0;
 }
